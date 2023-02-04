cbbc47550e815703029edac9030ec27e40b28491
Roland Kołodziej <roland@oferado.pl>
2023-02-04 21:46:28 +0100

    Environment improvements and setup

--- src/EventListener/ExceptionHandler.php
+++ src/EventListener/ExceptionHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Netborg\Fediverse\Api\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -12,9 +13,15 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 #[AsEventListener]
 class ExceptionHandler
 {
+    public function __construct(
+        private readonly LoggerInterface $logger
+    ) {
+    }
+
     public function __invoke(ExceptionEvent $exceptionEvent): void
     {
         $throwable = $exceptionEvent->getThrowable();
+        $this->logger->error($throwable->getMessage(), $throwable->getTrace());
 
         $httpStatus = match (true) {
             is_a($throwable, HttpException::class) => $throwable->getStatusCode(),
--- tests/Integration/AbstractKernelTestCase.php
+++ tests/Integration/AbstractKernelTestCase.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 
 namespace Netborg\Fediverse\Api\Tests\Integration;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 abstract class AbstractKernelTestCase extends KernelTestCase
 {
+    private ?EntityManagerInterface $entityManager;
+
     public function setUp(): void
     {
         self::bootKernel();
+
+        $this->entityManager = self::getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
     }
 }
